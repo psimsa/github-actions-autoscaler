aa905b5adc680e42e20fea9553cebf9d45c93206
Pavel Simsa <pavel@simsa.cz>
2022-04-18 20:05:05 +0200

    Initial

---
+++ AutoscalerApi/Program.cs
@@ -0,0 +1,50 @@
+using AutoscalerApi;
+using AutoscalerApi.Services;
+using AutoscalerApi.Workers;
+using Docker.DotNet;
+using ApplicationJsonSerializerContext = AutoscalerApi.Models.ApplicationJsonSerializerContext;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.custom.json", true);
+var appConfig = AppConfiguration.FromConfiguration(builder.Configuration);
+/*builder.Configuration.Bind(appConfig);*/
+
+// Add services to the container.
+if (appConfig.UseWebEndpoint)
+{
+    builder.Services.AddControllers().AddJsonOptions(options =>
+        options.JsonSerializerOptions.AddContext<ApplicationJsonSerializerContext>());
+
+    builder.Services.AddEndpointsApiExplorer();
+}
+
+builder.Services.AddSingleton<IDockerService, DockerService>();
+
+var dockerConfig = !string.IsNullOrWhiteSpace(appConfig.DockerHost)
+    ? new DockerClientConfiguration(new Uri(appConfig.DockerHost))
+    : new DockerClientConfiguration();
+builder.Services.AddSingleton(_ => dockerConfig.CreateClient());
+
+if (!string.IsNullOrWhiteSpace(appConfig.AzureStorage))
+{
+    builder.Services.AddHostedService<QueueMonitorWorker>();
+}
+
+builder.Services.AddSingleton(appConfig);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
+if (appConfig.UseWebEndpoint)
+{
+    app.UseAuthorization();
+    app.MapControllers();
+}
+
+app.Run();
---
+++ Playground/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+var _client = new DockerClientConfiguration().CreateClient();
+
+var containers = await _client.Containers.ListContainersAsync(new ContainersListParameters()
+{
+    Filters = new Dictionary<string, IDictionary<string, bool>>()
+    {
+        {
+            "label", new Dictionary<string, bool>()
+            {
+                {"autoscaler=true", true}
+            }
+        }
+    }
+});
+
+Console.WriteLine($"{containers.Count} containers found");
+var container = containers.FirstOrDefault();
+if (container == null) Environment.Exit(0);
+
+Console.WriteLine(container.Image);
