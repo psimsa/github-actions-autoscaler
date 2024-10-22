72225f27d9818d7fe850b9bdfddd43f7cad9d5ff
Pavel Simsa <pavel@simsa.cz>
2024-10-22 14:52:15 +0200

    Add enqueue endpoint (#166)

---
+++ AutoscalerApi/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.Json.Nodes;
+using Azure.Storage.Queues;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoscalerApi;
+
+public static class EndpointRouteBuilderExtensions
+{
+    public static IEndpointRouteBuilder MapWorkflowEndpoints(this IEndpointRouteBuilder builder)
+    {
+        var group = builder.MapGroup("workflow");
+        group
+            .MapGet("ping", () => Results.Ok(new { message = "Pong" }))
+            .WithName("Healthcheck")
+            .WithOpenApi();
+
+        group
+            .MapPost(
+                "enqueue-job",
+                async ([FromBody] JsonNode job, QueueClient queueClient) =>
+                {
+                    string messageText = job.ToJsonString();
+                    string base64MessageText = Convert.ToBase64String(
+                        System.Text.Encoding.UTF8.GetBytes(messageText)
+                    );
+                    var receipt = await queueClient.SendMessageAsync(base64MessageText);
+                    return Results.Ok();
+                }
+            )
+            .WithName("Enqueue Job")
+            .WithOpenApi();
+
+        return builder;
+    }
+}
