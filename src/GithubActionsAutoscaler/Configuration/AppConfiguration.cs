e5d00242fc8707f59daf05b013ab77ec7b73b9db
Pavel Simsa <pavel.simsa@vista.com>
2026-01-17 16:08:39 +0100

    refactor: fix nullable warnings in AppConfiguration
    - Initialized all properties with default values
    - Added null-coalescing operators for configuration values
    - Used C# 14 collection expressions
    - Removed commented out code

--- src/GithubActionsAutoscaler/Configuration/AppConfiguration.cs
+++ src/GithubActionsAutoscaler/Configuration/AppConfiguration.cs
@@ -9,17 +9,17 @@ public class AppConfiguration
     public string AzureStorage { get; set; } = "";
     public string AzureStorageQueue { get; set; } = "";
     public string DockerToken { get; set; } = "";
-    public string DockerImage { get; set; }
+    public string DockerImage { get; set; } = "myoung34/github-runner:latest";
     public string GithubToken { get; set; } = "";
     public int MaxRunners { get; set; }
     public string RepoWhitelistPrefix { get; set; } = "";
-    public string[] RepoWhitelist { get; set; } = Array.Empty<string>();
+    public string[] RepoWhitelist { get; set; } = [];
     public bool IsRepoWhitelistExactMatch { get; set; }
     public string RepoBlacklistPrefix { get; set; } = "";
-    public string[] RepoBlacklist { get; set; } = Array.Empty<string>();
+    public string[] RepoBlacklist { get; set; } = [];
     public bool IsRepoBlacklistExactMatch { get; set; }
     public string DockerHost { get; set; } = "";
-    public string[] Labels { get; set; } = Array.Empty<string>();
+    public string[] Labels { get; set; } = [];
     public string ApplicationInsightsConnectionString { get; set; } = "";
     public bool AutoCheckForImageUpdates { get; set; }
 
@@ -39,38 +39,39 @@ public class AppConfiguration
         };
 
         var architecture = RuntimeInformation.ProcessArchitecture.ToString().ToLowerInvariant();
-        /*var os = architecture switch
-        {
-            _ when RuntimeInformation.IsOSPlatform(OSPlatform.Windows) => "windows",
-            _ when RuntimeInformation.IsOSPlatform(OSPlatform.Linux) => "linux",
-            _ when RuntimeInformation.IsOSPlatform(OSPlatform.OSX) => "osx",
-            _ => ""
-        };*/
 
         return new AppConfiguration()
         {
-            AzureStorageQueue = configuration.GetValue<string>("AzureStorageQueue"),
-            AzureStorage = configuration.GetValue<string>("AzureStorage"),
+            AzureStorageQueue = configuration.GetValue<string>("AzureStorageQueue") ?? "",
+            AzureStorage = configuration.GetValue<string>("AzureStorage") ?? "",
             UseWebEndpoint = configuration.GetValue<bool>("UseWebEndpoint"),
-            DockerToken = configuration.GetValue<string>("DockerToken"),
-            GithubToken = configuration.GetValue<string>("GithubToken"),
+            DockerToken = configuration.GetValue<string>("DockerToken") ?? "",
+            GithubToken = configuration.GetValue<string>("GithubToken") ?? "",
             MaxRunners = maxRunners,
-            RepoWhitelistPrefix = configuration.GetValue<string>("RepoWhitelistPrefix"),
-            RepoWhitelist = configuration
-                .GetValue<string>("RepoWhitelist")
-                .Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
-                .Distinct()
-                .ToArray(),
+            RepoWhitelistPrefix = configuration.GetValue<string>("RepoWhitelistPrefix") ?? "",
+            RepoWhitelist =
+                configuration
+                    .GetValue<string>("RepoWhitelist")
+                    ?.Split(
+                        ',',
+                        StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries
+                    )
+                    .Distinct()
+                    .ToArray() ?? [],
             IsRepoWhitelistExactMatch = configuration.GetValue<bool>(
                 "IsRepoWhitelistExactMatch",
                 true
             ),
-            RepoBlacklistPrefix = configuration.GetValue<string>("RepoBlacklistPrefix"),
-            RepoBlacklist = configuration
-                .GetValue<string>("RepoBlacklist")
-                .Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
-                .Distinct()
-                .ToArray(),
+            RepoBlacklistPrefix = configuration.GetValue<string>("RepoBlacklistPrefix") ?? "",
+            RepoBlacklist =
+                configuration
+                    .GetValue<string>("RepoBlacklist")
+                    ?.Split(
+                        ',',
+                        StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries
+                    )
+                    .Distinct()
+                    .ToArray() ?? [],
             IsRepoBlacklistExactMatch = configuration.GetValue<bool>("IsRepoBlacklistExactMatch"),
             DockerHost =
                 configuration.GetValue<string>("DockerHost") ?? "unix:/var/run/docker.sock",
@@ -81,21 +82,19 @@ public class AppConfiguration
                     .Split(
                         ',',
                         StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries
-                    ) ?? Array.Empty<string>()
+                    ) ?? []
             )
                 .Concat(
-                    new[]
-                    {
+                    [
                         "self-hosted",
                         architecture,
                         // os
-                    }
+                    ]
                 )
                 .Distinct()
                 .ToArray(),
-            ApplicationInsightsConnectionString = configuration.GetValue<string>(
-                "APPLICATIONINSIGHTS_CONNECTION_STRING"
-            ),
+            ApplicationInsightsConnectionString =
+                configuration.GetValue<string>("APPLICATIONINSIGHTS_CONNECTION_STRING") ?? "",
             DockerImage =
                 configuration.GetValue<string>("DockerImage") ?? "myoung34/github-runner:latest",
             AutoCheckForImageUpdates = configuration.GetValue<bool>(
